f0acbb952a3006346dfe487127264ea62253d4be
AleksHome <aa.polushkin@yandex.ru>
2023-10-14 12:04:13 +0300

    Add ChatViewModel and MessageViewModel

---
+++ SimpleSocialNetwork.BLL/ViewModels/Account/ChatViewModel.cs
@@ -0,0 +1,25 @@
+﻿using SimpleSocialNetwork.DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSocialNetwork.BLL.ViewModels.Account
+{
+    public class ChatViewModel
+    {
+        public User You { get; set; }
+
+        public User ToWhom { get; set; }
+
+        public List<Message> History { get; set; }
+
+        public MessageViewModel NewMessage { get; set; }
+
+        public ChatViewModel()
+        {
+            NewMessage = new MessageViewModel();
+        }
+    }
+}
